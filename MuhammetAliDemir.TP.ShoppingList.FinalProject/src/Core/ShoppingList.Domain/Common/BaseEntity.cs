eae1a48a8f813d6cd6cb8a067848c0a2d95dab5b
mhtaldmr <mht.alidemir@gmail.com>
2022-06-25 15:23:05 +0300

    Domain Project and BaseEntity created.

---
+++ MuhammetAliDemir.TP.ShoppingList.FinalProject/src/Core/ShoppingList.Domain/Common/BaseEntity.cs
@@ -0,0 +1,11 @@
+﻿namespace ShoppingList.Domain.Common
+{
+    public class BaseEntity
+    {
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public bool IsActive { get; set; } = true;
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+        public DateTime? UpdatedAt { get; set; }
+    }
+}
