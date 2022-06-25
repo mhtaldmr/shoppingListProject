49d706f4e38f58eb1be152f0dcb5c26e2a0d5a9b
mhtaldmr <mht.alidemir@gmail.com>
2022-06-25 16:12:28 +0300

    Generic Repository created.

---
+++ MuhammetAliDemir.TP.ShoppingList.FinalProject/src/Core/ShoppingList.Application/Interfaces/Repositories/IRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace ShoppingList.Application.Interfaces.Repositories
+{
+    public interface IRepository<T>
+    {
+        Task<IEnumerable<T>> GetAll();
+        Task<T> GetById(int id);
+        void Create(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+    }
+}
---
+++ MuhammetAliDemir.TP.ShoppingList.FinalProject/src/Infrastructure/ShoppingList.Infrastructure/Repositories/Repository.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ShoppingList.Application.Interfaces.Repositories;
+using ShoppingList.Domain.Common;
+using ShoppingList.Infrastructure.Persistence.DbContext;
+
+namespace ShoppingList.Infrastructure.Repositories
+{
+    public class Repository<T> : IRepository<T> where T : BaseEntity
+    {
+        private readonly ShoppingListDbContext _context;
+
+        public Repository(ShoppingListDbContext context) => _context = context;
+
+        public void Create(T entity)
+        {
+            _context.Set<T>().Add(entity);
+            _context.SaveChanges();
+        }
+
+        public void Update(T entity)
+        {
+            _context.Set<T>().Update(entity);
+            _context.SaveChanges();
+        }
+
+        public void Delete(T entity)
+        {
+            _context.Set<T>().Remove(entity);
+            _context.SaveChanges();
+        }
+
+        public async Task<IEnumerable<T>> GetAll() => await _context.Set<T>().ToListAsync();
+
+        public async Task<T> GetById(int id) => await _context.Set<T>().FirstOrDefaultAsync(x => x.Id == id);
+
+    }
+}
