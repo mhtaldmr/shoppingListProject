5e77e3284fd8271d0884c8150873535d6a3a4edb
mhtaldmr <mht.alidemir@gmail.com>
2022-06-25 15:34:07 +0300

    Table relations are cretaed.

---
+++ MuhammetAliDemir.TP.ShoppingList.FinalProject/src/Core/ShoppingList.Domain/Entities/ListCategory.cs
@@ -0,0 +1,11 @@
+﻿namespace ShoppingList.Domain.Entities
+{
+    public class ListCategory
+    {
+        public int Id { get; set; }
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+        public int ListId { get; set; }
+        public List List { get; set; }
+    }
+}
---
+++ MuhammetAliDemir.TP.ShoppingList.FinalProject/src/Core/ShoppingList.Domain/Entities/ListCategotyItem.cs
@@ -0,0 +1,13 @@
+﻿namespace ShoppingList.Domain.Entities
+{
+    public class ListCategotyItem
+    {
+        public int ListCategoryId { get; set; }
+        public ListCategory ListCategory { get; set; }
+        public int ItemId { get; set; }
+        public Item Item { get; set; }
+        public int UnitOfMaterialId { get; set; }
+        public UnitofMaterial UnitofMaterial { get; set; }
+        public int Quantity { get; set; }
+    }
+}
