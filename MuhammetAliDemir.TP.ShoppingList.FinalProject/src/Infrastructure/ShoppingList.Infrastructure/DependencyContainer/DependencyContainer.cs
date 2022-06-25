b0047b31de8c2fd27700f241af4a6931bc6edd11
mhtaldmr <mht.alidemir@gmail.com>
2022-06-25 15:49:23 +0300

    DBContext registered to container.

---
+++ MuhammetAliDemir.TP.ShoppingList.FinalProject/src/Infrastructure/ShoppingList.Infrastructure/DependencyContainer/DependencyContainer.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using ShoppingList.Infrastructure.Persistence.DbContext;
+
+namespace ShoppingList.Infrastructure.DependencyContainer
+{
+    public static class DependencyContainer
+    {
+        public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            //DbContext configuration
+            var connectionString = configuration.GetConnectionString("Default");
+            services.AddDbContext<ShoppingListDbContext>(options => options.UseSqlServer(connectionString));
+
+
+            //Interface contracts
+        
+
+            return services;
+        }
+    }
+}
--- MuhammetAliDemir.TP.ShoppingList.FinalProject/src/Web/ShoppingList.Server/Program.cs
+++ MuhammetAliDemir.TP.ShoppingList.FinalProject/src/Web/ShoppingList.Server/Program.cs
@@ -1,6 +1,12 @@
+using ShoppingList.Infrastructure.DependencyContainer;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddInfrastructureServices(builder.Configuration);
+
+
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
