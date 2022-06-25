3132f8a76603dd114f67602d7a3a37f5b10a3c78
mhtaldmr <mht.alidemir@gmail.com>
2022-06-25 16:33:31 +0300

    User repository fulfilled.

--- MuhammetAliDemir.TP.ShoppingList.FinalProject/src/Core/ShoppingList.Application/Interfaces/Repositories/IUserRepository.cs
+++ MuhammetAliDemir.TP.ShoppingList.FinalProject/src/Core/ShoppingList.Application/Interfaces/Repositories/IUserRepository.cs
@@ -1,6 +1,12 @@
-﻿namespace ShoppingList.Application.Interfaces.Repositories
+﻿using ShoppingList.Domain.Entities;
+
+namespace ShoppingList.Application.Interfaces.Repositories
 {
     public interface IUserRepository
     {
+        Task<User> GetById(string id);
+        Task<IEnumerable<User>> GetAll();
+        void Create(User user);
+        void Delete(User user);
     }
 }
--- MuhammetAliDemir.TP.ShoppingList.FinalProject/src/Infrastructure/ShoppingList.Infrastructure/Repositories/UserRepository.cs
+++ MuhammetAliDemir.TP.ShoppingList.FinalProject/src/Infrastructure/ShoppingList.Infrastructure/Repositories/UserRepository.cs
@@ -1,9 +1,27 @@
-﻿namespace ShoppingList.Infrastructure.Repositories
+﻿using Microsoft.EntityFrameworkCore;
+using ShoppingList.Application.Interfaces.Repositories;
+using ShoppingList.Domain.Entities;
+using ShoppingList.Infrastructure.Persistence.DbContext;
+
+namespace ShoppingList.Infrastructure.Repositories
 {
-    public class UserRepository
+    public class UserRepository : IUserRepository
     {
-        public UserRepository()
+        private readonly ShoppingListDbContext _context;
+
+        public UserRepository(ShoppingListDbContext context) => _context = context;
+
+        public async Task<User> GetById(string id)
         {
+            var user = await _context.Users.FirstOrDefaultAsync(u => u.Id == id);
+            return user is null ? throw new ArgumentNullException("Userid does not exist!") : user;
         }
+
+        public async Task<IEnumerable<User>> GetAll() => await _context.Users.ToListAsync();
+
+        public void Create(User user) => _context.Users.Add(user);
+      
+        public void Delete(User user) => _context.Users.Remove(user);
+        
     }
 }
